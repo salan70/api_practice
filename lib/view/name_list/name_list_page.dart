5fdc32605918cf0940ba08976d315942eb41fb27
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-07-13 13:30:39 +0900

    並び替え用のテキストボタンを作成

--- lib/view/name_list/name_list_page.dart
+++ lib/view/name_list/name_list_page.dart
@@ -15,34 +15,48 @@ class NameListPage extends ConsumerWidget {
       ),
       body: ColoredBox(
         color: Colors.black12,
-        child: parsonDataList.when(
-          data: (parsonDataList) {
-            return ListView.builder(
-              itemCount: parsonDataList.length,
-              itemBuilder: (BuildContext context, int index) {
-                final dynamic parsonData = parsonDataList[index];
+        child: Column(
+          children: [
+            parsonDataList.when(
+              data: (parsonDataList) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: parsonDataList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final dynamic parsonData = parsonDataList[index];
 
-                return Card(
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        parsonData['avatar'].toString(),
+                    return Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            parsonData['avatar'].toString(),
+                          ),
+                        ),
+                        title: Text(
+                          '${parsonData['first_name']} ${parsonData['last_name']}',
+                        ),
+                        subtitle: Text(
+                          '${parsonData['email']}',
+                        ),
+                        trailing: Text('id: ${parsonData['id']}'),
                       ),
-                    ),
-                    title: Text(
-                      '${parsonData['first_name']} ${parsonData['last_name']}',
-                    ),
-                    subtitle: Text(
-                      '${parsonData['email']}',
-                    ),
-                    trailing: Text('id: ${parsonData['id']}'),
-                  ),
+                    );
+                  },
                 );
               },
-            );
-          },
-          error: (error, stack) => Text('Error: $error'),
-          loading: () => const CircularProgressIndicator(),
+              error: (error, stack) => Text('Error: $error'),
+              loading: () => const CircularProgressIndicator(),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('idの昇順で並び替え（デフォルト）'),
+            ),
+            TextButton(onPressed: () {}, child: const Text('名前の長さの昇順で並び替え'))
+          ],
         ),
       ),
     );
