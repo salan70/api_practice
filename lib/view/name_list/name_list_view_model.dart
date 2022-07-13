c88b5ee066d469211e6b4b78af5447425993e9dd
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-07-13 14:27:24 +0900

    first_nameでソートする関数を作成

--- lib/view/name_list/name_list_view_model.dart
+++ lib/view/name_list/name_list_view_model.dart
@@ -1,3 +1,10 @@
-// class NameListViewModel {
-//   List<>
-// }
+import 'package:api_practice/model/person_model.dart';
+
+class NameListViewModel {
+  List<Person> sortFirstName(List<Person> personDataList) {
+    final result = personDataList
+      ..sort((a, b) => a.first_name.compareTo(b.first_name));
+
+    return result;
+  }
+}
