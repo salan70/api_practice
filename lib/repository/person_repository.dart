fda4d9367904331341b67487368250b6c6aa2fda
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-07-13 17:09:02 +0900

    コメントを追加。

--- lib/repository/person_repository.dart
+++ lib/repository/person_repository.dart
@@ -5,13 +5,19 @@ class PersonRepository {
   Future<List<Person>> fetchParsonDataList() async {
     List<dynamic> responseDataList;
     final personDataList = <Person>[];
-    final response = await Dio()
-        .get<Map<dynamic, dynamic>>('https://reqres.in/api/users?page=2');
+
+    // 利用するAPIのURL
+    const url = 'https://reqres.in/api/users?page=2';
+    final response = await Dio().get<Map<dynamic, dynamic>>(url);
 
     if (response.statusCode == 200) {
+      // null check
       if (response.data != null) {
+        // responseのdataを一旦、dynamic型のListとして格納
         responseDataList = response.data!['data'] as List<dynamic>;
+
         for (final responseData in responseDataList) {
+          // Listの要素を1つ1つPerson(自作の型)に変換し、返すListに追加していく
           personDataList
               .add(Person.fromJson(responseData as Map<String, dynamic>));
         }
